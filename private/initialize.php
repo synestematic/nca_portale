e8a42e7a263b1c6049f500fab227e57a91d617c1
Fed Rizzo <synestematic@gmail.com>
2017-02-03 17:14:10 +0100

    - CHanges classes for Piva and Date - Removes useless UPLOAD_DIR and adds TMP_DIR constants

--- private/initialize.php
+++ private/initialize.php
@@ -3,7 +3,7 @@
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 defined("SITE_ROOT") ? null : define("SITE_ROOT", DS."var".DS."www".DS."html");
 defined("LIB_PATH") ? null : define("LIB_PATH", SITE_ROOT.DS."private");
-defined("UPLOAD_PATH") ? null : define("UPLOAD_PATH", LIB_PATH.DS."uploads");
+defined("TMP_PATH") ? null : define("TMP_PATH", SITE_ROOT.DS."public".DS."tmp");
 
 // the order of the requires IS VERY IMPORTANT
 require_once(LIB_PATH.DS."config.php");
@@ -14,17 +14,12 @@ require_once(LIB_PATH.DS."class.Database.inc");
 require_once(LIB_PATH.DS."class.User.inc");
 require_once(LIB_PATH.DS."class.Branch.inc");
 require_once(LIB_PATH.DS."class.Chiamata.inc");
-require_once(LIB_PATH.DS."class.Piva.inc");
+require_once(LIB_PATH.DS."class.Merchant.inc");
 require_once(LIB_PATH.DS."class.Dept.inc");
 require_once(LIB_PATH.DS."class.Session.inc");
 
 require_once(LIB_PATH.DS."class.SqlTable.inc");
 require_once(LIB_PATH.DS."class.Dropdown.inc");
-require_once(LIB_PATH.DS."class.DropdownMins.inc");
-require_once(LIB_PATH.DS."class.DropdownHours.inc");
-require_once(LIB_PATH.DS."class.DropdownDays.inc");
-require_once(LIB_PATH.DS."class.DropdownMonths.inc");
-require_once(LIB_PATH.DS."class.DropdownYears.inc");
-require_once(LIB_PATH.DS."class.DropdownSymbols.inc");
+require_once(LIB_PATH.DS."class.Time.inc");
 
 ?>
