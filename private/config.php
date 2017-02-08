b4183660a785d0d5ee8afbb4f618ef833d92d0da
Fed Rizzo <synestematic@gmail.com>
2017-02-08 09:51:47 +0100

    - adds new Finproget FTTH server in config.php and makes it the default. - adds NAS connection constants to config.php .

--- private/config.php
+++ private/config.php
@@ -4,10 +4,16 @@ defined("DB_USER") ? null : define("DB_USER", "natterbox");
 defined("DB_PASS") ? null : define("DB_PASS", "q1w2e3r4");
 defined("DB_DB") ? null : define("DB_DB", "nca");
 
-defined("FP_SERVER") ? null : define("FP_SERVER", "79.2.100.143");
+// defined("FP_SERVER") ? null : define("FP_SERVER", "79.2.100.143"); // ADSL Tim
+defined("FP_SERVER") ? null : define("FP_SERVER", "93.42.121.142"); // FTTH Fastweb
 defined("FP_USER") ? null : define("FP_USER", "Auto1user");
 defined("FP_PASS") ? null : define("FP_PASS", "vyHuY6D6");
 defined("FP_DB") ? null : define("FP_DB", "migrazione3");
+
+defined("NAS_SERVER") ? null : define("NAS_SERVER", "10.4.4.250");
+defined("NAS_USER") ? null : define("NAS_USER", "portale");
+defined("NAS_PASS") ? null : define("NAS_PASS", "76837683");
+defined("NAS_SHARE") ? null : define("NAS_SHARE", "FTP_BRANCHES");
 
 $upload_errors = array(
 	// http://www.php.net/manual/en/features.file-upload.errors.php
