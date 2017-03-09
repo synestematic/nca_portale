0d6ca96b7414b74e4365ae1f14dd5451957e15cc
Fed Rizzo <synestematic@gmail.com>
2017-03-10 00:16:36 +0100

    - Search() class is now the more generic Form() parentClass which   contains Search() and Block() subclasses - Time() class now checks dates using a regex - String() class is now using ucwords instead of ucfirst.

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,7 @@
 <?php
+// setlocale(LC_TIME, 'it_IT');
+// setlocale(LC_ALL, 'it_IT');
+
 // DIRECTORY_SEPARATOR is / for unix and the other slash for windows
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 defined("SITE_ROOT") ? null : define("SITE_ROOT", DS."var".DS."www".DS."html");
@@ -21,7 +24,7 @@ require_once(LIB_PATH.DS."class.Veicolo.inc");
 
 require_once(LIB_PATH.DS."class.SqlTable.inc");
 require_once(LIB_PATH.DS."class.Dropdown.inc");
-require_once(LIB_PATH.DS."class.Search.inc");
+require_once(LIB_PATH.DS."class.Form.inc");
 require_once(LIB_PATH.DS."class.Validation.inc");
 require_once(LIB_PATH.DS."class.Time.inc");
 require_once(LIB_PATH.DS."class.String.inc");
