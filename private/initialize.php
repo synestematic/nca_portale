c9ab62f7f2a4ee1e77588c9a2741c2a3402de5af
Fed Rizzo <synestematic@gmail.com>
2017-03-16 09:04:40 +0100

    - Removes unused STATIC classes Merchant() and Veicolo() in favour of   DYNAMIC SqlTable() class - admin removes obsolete links to old fp scripts.

--- private/initialize.php
+++ private/initialize.php
@@ -14,18 +14,19 @@ require_once(LIB_PATH.DS."functions.php");
 
 require_once(LIB_PATH.DS."class.Database.inc");
 
-require_once(LIB_PATH.DS."class.User.inc");
 require_once(LIB_PATH.DS."class.Session.inc");
+require_once(LIB_PATH.DS."class.User.inc");
 require_once(LIB_PATH.DS."class.Branch.inc");
-require_once(LIB_PATH.DS."class.Chiamata.inc");
-require_once(LIB_PATH.DS."class.Merchant.inc");
 require_once(LIB_PATH.DS."class.Dept.inc");
-require_once(LIB_PATH.DS."class.Veicolo.inc");
 
+require_once(LIB_PATH.DS."class.Chiamata.inc");
 require_once(LIB_PATH.DS."class.SqlTable.inc");
+
 require_once(LIB_PATH.DS."class.Dropdown.inc");
 require_once(LIB_PATH.DS."class.Form.inc");
+
 require_once(LIB_PATH.DS."class.Validation.inc");
+
 require_once(LIB_PATH.DS."class.Time.inc");
 require_once(LIB_PATH.DS."class.String.inc");
 
